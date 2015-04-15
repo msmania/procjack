0f49922af2597d10e120db8bed37a5af2c16c4fb
tokikuch <k.toshihito@yahoo.de>
2015-04-14 22:04:43 -0700

    disassembled stuff

---
+++ expeb/expeb32.asm
@@ -0,0 +1,382 @@
+[org 0]
+[bits 32]
+
+GetImageBase:
+push ebx
+push esi
+mov esi,[esp+0xc]
+mov eax,[esi+0xa08]
+mov ebx,[eax+0xc]
+add ebx,byte +0x14
+mov ecx,[ebx]
+cmp ecx,ebx
+jz label0xe0
+
+push edi
+mov edi,[esi+0xa0c]
+
+label0x23:
+mov eax,[ecx+0x28]
+test edi,edi
+jnz label0x44
+
+cmp dword [eax],0x74006e
+jnz label0x44
+
+cmp dword [eax+0x4],0x6c0064
+jnz label0x44
+
+cmp dword [eax+0x8],0x2e006c
+jz label0x60
+
+label0x44:
+mov edx,[eax]
+cmp edx,0x54004e
+jnz label0x6b
+
+cmp dword [eax+0x4],0x4c0044
+jnz label0x6b
+
+cmp dword [eax+0x8],0x2e004c
+jnz label0x6b
+
+label0x60:
+mov edi,[ecx+0x10]
+mov [esi+0xa0c],edi
+jmp short label0xd5
+
+label0x6b:
+cmp dword [esi+0xa10],byte +0x0
+jnz label0xa0
+
+cmp edx,0x45004b
+jnz label0xa0
+
+cmp dword [eax+0x4],0x4e0052
+jnz label0xa0
+
+cmp dword [eax+0x8],0x4c0045
+jnz label0xa0
+
+cmp dword [eax+0xc],0x320033
+jnz label0xa0
+
+cmp dword [eax+0x10],0x44002e
+jz label0xcc
+
+label0xa0:
+cmp edx,0x65006b
+jnz label0xd5
+
+cmp dword [eax+0x4],0x6e0072
+jnz label0xd5
+
+cmp dword [eax+0x8],0x6c0065
+jnz label0xd5
+
+cmp dword [eax+0xc],0x320033
+jnz label0xd5
+
+cmp dword [eax+0x10],0x64002e
+jnz label0xd5
+
+label0xcc:
+mov eax,[ecx+0x10]
+mov [esi+0xa10],eax
+
+label0xd5:
+mov ecx,[ecx]
+cmp ecx,ebx
+jnz label0x23
+
+pop edi
+
+label0xe0:
+pop esi
+pop ebx
+ret
+
+
+
+
+int3
+int3
+int3
+int3
+int3
+int3
+int3
+int3
+int3
+int3
+int3
+int3
+int3
+
+
+mov eax,[fs:0x30]
+ret
+
+int3
+int3
+int3
+int3
+int3
+int3
+int3
+int3
+int3
+
+
+GetProcAddress:
+sub esp,byte +0xc
+mov eax,0x5a4d
+push ebx
+mov ebx,[esp+0x14]
+cmp [ebx],ax
+jnz label0x330
+
+mov edx,[ebx+0x3c]
+cmp dword [edx+ebx],0x4550
+jnz label0x330
+
+movzx ecx,word [edx+ebx+0x4]
+push edi
+mov edi,0x14c
+cmp cx,di
+jz label0x144
+
+mov eax,0x8664
+cmp cx,ax
+jnz label0x32f
+
+label0x144:
+cmp cx,di
+push esi
+mov esi,0x60
+mov eax,0x70
+cmovz eax,esi
+add eax,edx
+xor edi,edi
+mov eax,[eax+ebx+0x18]
+add eax,ebx
+mov [esp+0xc],eax
+mov ecx,[eax+0x20]
+mov esi,[eax+0x24]
+mov edx,[eax+0x1c]
+add ecx,ebx
+add esi,ebx
+add edx,ebx
+mov [esp+0x10],ecx
+mov [esp+0x14],esi
+mov [esp+0x1c],edx
+cmp [eax+0x18],edi
+jna label0x32e
+
+mov edx,[esp+0x20]
+push ebp
+mov ebp,[edx+0xa14]
+
+label0x192:
+mov eax,[ecx+edi*4]
+movzx ecx,word [esi+edi*2]
+mov esi,[esp+0x20]
+add eax,ebx
+mov esi,[esi+ecx*4]
+add esi,ebx
+test ebp,ebp
+jnz label0x1d5
+
+cmp dword [eax],0x64616f4c
+jnz label0x1d5
+
+cmp dword [eax+0x4],0x7262694c
+jnz label0x1d5
+
+cmp dword [eax+0x8],0x57797261
+jnz label0x1d5
+
+cmp byte [eax+0xc],0x0
+jnz label0x1d5
+
+mov ebp,esi
+mov [edx+0xa14],esi
+jmp dword label0x317
+
+label0x1d5:
+cmp dword [edx+0xa18],byte +0x0
+jnz label0x203
+
+cmp dword [eax],0x65657246
+jnz label0x203
+
+cmp dword [eax+0x4],0x7262694c
+jnz label0x203
+
+cmp dword [eax+0x8],0x797261
+jnz label0x203
+
+mov [edx+0xa18],esi
+jmp dword label0x317
+
+label0x203:
+cmp dword [edx+0xa1c],byte +0x0
+jnz label0x242
+
+cmp dword [eax],0x50746547
+jnz label0x242
+
+cmp dword [eax+0x4],0x41636f72
+jnz label0x242
+
+cmp dword [eax+0x8],0x65726464
+jnz label0x242
+
+mov ecx,[eax+0xc]
+and ecx,0xffffff
+cmp ecx,0x7373
+jnz label0x242
+
+mov [edx+0xa1c],esi
+jmp dword label0x317
+
+label0x242:
+cmp dword [edx+0xa20],byte +0x0
+jnz label0x276
+
+cmp dword [eax],0x61657243
+jnz label0x276
+
+cmp dword [eax+0x4],0x68546574
+jnz label0x276
+
+cmp dword [eax+0x8],0x64616572
+jnz label0x276
+
+cmp byte [eax+0xc],0x0
+jnz label0x276
+
+mov [edx+0xa20],esi
+jmp dword label0x317
+
+label0x276:
+cmp dword [edx+0xa24],byte +0x0
+jnz label0x2b1
+
+cmp dword [eax],0x456c7452
+jnz label0x2b1
+
+cmp dword [eax+0x4],0x55746978
+jnz label0x2b1
+
+cmp dword [eax+0x8],0x54726573
+jnz label0x2b1
+
+cmp dword [eax+0xc],0x61657268
+jnz label0x2b1
+
+cmp word [eax+0x10],byte +0x64
+jnz label0x2b1
+
+mov [edx+0xa24],esi
+jmp short label0x317
+
+label0x2b1:
+cmp dword [edx+0xa28],byte +0x0
+jnz label0x2ee
+
+cmp dword [eax],0x74696157
+jnz label0x2ee
+
+cmp dword [eax+0x4],0x53726f46
+jnz label0x2ee
+
+cmp dword [eax+0x8],0x6c676e69
+jnz label0x2ee
+
+cmp dword [eax+0xc],0x6a624f65
+jnz label0x2ee
+
+cmp dword [eax+0x10],0x746365
+jnz label0x2ee
+
+mov [edx+0xa28],esi
+jmp short label0x317
+
+label0x2ee:
+cmp dword [edx+0xa2c],byte +0x0
+jnz label0x317
+
+cmp dword [eax],0x736f6c43
+jnz label0x317
+
+cmp dword [eax+0x4],0x6e614865
+jnz label0x317
+
+cmp dword [eax+0x8],0x656c64
+jnz label0x317
+
+mov [edx+0xa2c],esi
+
+label0x317:
+mov eax,[esp+0x10]
+mov ecx,[esp+0x14]
+mov esi,[esp+0x18]
+inc edi
+cmp edi,[eax+0x18]
+jc label0x192
+
+pop ebp
+
+label0x32e:
+pop esi
+
+label0x32f:
+pop edi
+
+label0x330:
+pop ebx
+add esp,byte +0xc
+ret
+
+int3
+int3
+int3
+int3
+int3
+int3
+int3
+int3
+int3
+int3
+int3
+
+ShellCode:
+mov eax,[fs:0x30]
+push esi
+mov esi,[esp+0x8]
+push esi
+mov [esi+0xa08],eax
+call GetImageBase
+
+push esi
+push dword [esi+0xa0c]
+call GetProcAddress
+
+push esi
+push dword [esi+0xa10]
+call GetProcAddress
+
+add esp,byte +0x14
+mov eax,0x2a
+pop esi
+ret
+
+
+int3
+int3
+int3
+int3
+int3
+int3
+int3
---
+++ expeb/expeb64.asm
@@ -0,0 +1,343 @@
+[org 0]
+[bits 64]
+
+GetImageBase:
+mov rax,[rcx+0xa08]
+mov r8,rcx
+mov r9,[rax+0x18]
+mov rdx,[r9+0x20]
+add r9,byte +0x20
+cmp rdx,r9
+jz label0xeb
+
+mov r10,[rcx+0xa10]
+
+label0x26:
+mov rax,[rdx+0x50]
+test r10,r10
+jnz label0x49
+
+cmp dword [rax],0x74006e
+jnz label0x49
+
+cmp dword [rax+0x4],0x6c0064
+jnz label0x49
+
+cmp dword [rax+0x8],0x2e006c
+jz label0x65
+
+label0x49:
+mov ecx,[rax]
+cmp ecx,0x54004e
+jnz label0x72
+
+cmp dword [rax+0x4],0x4c0044
+jnz label0x72
+
+cmp dword [rax+0x8],0x2e004c
+jnz label0x72
+
+label0x65:
+mov r10,[rdx+0x20]
+mov [r8+0xa10],r10
+jmp label0xdf
+
+label0x72:
+cmp qword [r8+0xa18],byte +0x0
+jnz label0xa8
+
+cmp ecx,0x45004b
+jnz label0xa8
+
+cmp dword [rax+0x4],0x4e0052
+jnz label0xa8
+
+cmp dword [rax+0x8],0x4c0045
+jnz label0xa8
+
+cmp dword [rax+0xc],0x320033
+jnz label0xa8
+
+cmp dword [rax+0x10],0x44002e
+jz label0xd4
+
+label0xa8:
+cmp ecx,0x65006b
+jnz label0xdf
+
+cmp dword [rax+0x4],0x6e0072
+jnz label0xdf
+
+cmp dword [rax+0x8],0x6c0065
+jnz label0xdf
+
+cmp dword [rax+0xc],0x320033
+jnz label0xdf
+
+cmp dword [rax+0x10],0x64002e
+jnz label0xdf
+
+label0xd4:
+mov rax,[rdx+0x20]
+mov [r8+0xa18],rax
+
+label0xdf:
+mov rdx,[rdx]
+cmp rdx,r9
+jz label0x26
+
+label0xeb:
+rep ret
+
+
+int3
+int3
+int3
+
+
+mov rax,[gs:0x30]
+mov rax,[rax+0x60]
+ret
+
+
+int3
+int3
+
+
+GetProcAddress:
+sub rsp,byte +0x8
+mov eax,0x5a4d
+mov r10,rcx
+cmp [rcx],ax
+jz label0x355
+
+mov r9d,[rcx+0x3c]
+cmp dword [r9+rcx],0x4550
+jz label0x355
+
+movzx r8d,word [r9+rcx+0x4]
+mov r11d,0x14c
+cmp r8w,r11w
+jz label0x148
+
+mov eax,0x8664
+cmp r8w,ax
+jz label0x355
+
+label0x148:
+mov [rsp+0x10],rbx
+cmp r8w,r11w
+mov [rsp+0x20],rsi
+mov [rsp],rdi
+mov eax,0x70
+mov ecx,0x60
+cmovz eax,ecx
+xor r11d,r11d
+add rax,r9
+mov r9d,[rax+r10+0x18]
+add r9,r10
+mov ebx,[r9+0x20]
+mov edi,[r9+0x24]
+mov esi,[r9+0x1c]
+add rbx,r10
+add rdi,r10
+add rsi,r10
+cmp [r9+0x18],r11d
+jna label0x347
+
+mov [rsp+0x18],rbp
+mov rbp,[rdx+0xa20]
+
+label0x1a0:
+movzx ecx,word [rdi+r11*2]
+mov eax,[rbx+r11*4]
+mov r8d,[rsi+rcx*4]
+add rax,r10
+add r8,r10
+test rbp,rbp
+jnz label0x1e7
+
+cmp dword [rax],0x64616f4c
+jnz label0x1e7
+
+cmp dword [rax+0x4],0x7262694c
+jnz label0x1e7
+
+cmp dword [rax+0x8],0x57797261
+jnz label0x1e7
+
+cmp [rax+0xc],bpl
+jnz label0x1e7
+
+mov rbp,r8
+mov [rdx+0xa20],r8
+jmp label0x335
+
+label0x1e7:
+cmp qword [rdx+0xa28],byte +0x0
+jnz label0x217
+
+cmp dword [rax],0x65657246
+jnz label0x217
+
+cmp dword [rax+0x4],0x7262694c
+jnz label0x217
+
+cmp dword [rax+0x8],0x797261
+jnz label0x217
+
+mov [rdx+0xa28],r8
+jmp qword 0x335
+
+label0x217:
+cmp qword [rdx+0xa30],byte +0x0
+jnz label0x258
+
+cmp dword [rax],0x50746547
+jnz label0x258
+
+cmp dword [rax+0x4],0x41636f72
+jnz label0x258
+
+cmp dword [rax+0x8],0x65726464
+jnz label0x258
+
+mov ecx,[rax+0xc]
+and ecx,0xffffff
+cmp ecx,0x7373
+jnz label0x258
+
+mov [rdx+0xa30],r8
+jmp label0x335
+
+label0x258:
+cmp qword [rdx+0xa38],byte +0x0
+jnz label0x28e
+
+cmp dword [rax],0x61657243
+jnz label0x28e
+
+cmp dword [rax+0x4],0x68546574
+jnz label0x28e
+
+cmp dword [rax+0x8],0x64616572
+jnz label0x28e
+
+cmp byte [rax+0xc],0x0
+jnz label0x28e
+
+mov [rdx+0xa38],r8
+jmp label0x335
+
+label0x28e:
+cmp qword [rdx+0xa40],byte +0x0
+jnz label0x2cb
+
+cmp dword [rax],0x456c7452
+jnz label0x2cb
+
+cmp dword [rax+0x4],0x55746978
+jnz label0x2cb
+
+cmp dword [rax+0x8],0x54726573
+jnz label0x2cb
+
+cmp dword [rax+0xc],0x61657268
+jnz label0x2cb
+
+cmp word [rax+0x10],byte +0x64
+jnz label0x2cb
+
+mov [rdx+0xa40],r8
+jmp label0x335
+
+label0x2cb:
+cmp qword [rdx+0xa48],byte +0x0
+jnz label0x30a
+
+cmp dword [rax],0x74696157
+jnz label0x30a
+
+cmp dword [rax+0x4],0x53726f46
+jnz label0x30a
+
+cmp dword [rax+0x8],0x6c676e69
+jnz label0x30a
+
+cmp dword [rax+0xc],0x6a624f65
+jnz label0x30a
+
+cmp dword [rax+0x10],0x746365
+jnz label0x30a
+
+mov [rdx+0xa48],r8
+jmp label0x335
+
+label0x30a:
+cmp qword [rdx+0xa50],byte +0x0
+jnz label0x335
+
+cmp dword [rax],0x736f6c43
+jnz label0x335
+
+cmp dword [rax+0x4],0x6e614865
+jnz label0x335
+
+cmp dword [rax+0x8],0x656c64
+jnz label0x335
+
+mov [rdx+0xa50],r8
+
+label0x335:
+inc r11d
+cmp r11d,[r9+0x18]
+jc label0x1a0
+
+mov rbp,[rsp+0x18]
+
+label0x347:
+mov rsi,[rsp+0x20]
+mov rbx,[rsp+0x10]
+mov rdi,[rsp]
+
+label0x355:
+add rsp,byte +0x8
+ret
+
+
+int3
+int3
+int3
+int3
+int3
+int3
+
+
+ShellCode:
+push rbx
+sub rsp,byte +0x20
+mov rax,[gs:0x30]
+mov rbx,rcx
+mov rdx,[rax+0x60]
+mov [rcx+0xa08],rdx
+call GetImageBase
+
+mov rcx,[rbx+0xa10]
+mov rdx,rbx
+call GetProcAddress
+
+mov rcx,[rbx+0xa18]
+mov rdx,rbx
+call GetProcAddress
+
+mov eax,0x2a
+add rsp,byte +0x20
+pop rbx
+ret
+
+
+int3
+int3
+int3
+int3
+int3
