f013637f627eabb38f2c2e0aad40ba2cf5d23ebb
Toshihito Kikuchi <k.toshihito@yahoo.de>
2018-07-15 18:59:39 -0700

    Add 32bit Clove

---
+++ clove/src/detour32.asm
@@ -0,0 +1,74 @@
+BITS 32
+
+;global Clove_Start
+;global Clove_End
+
+section .data
+Counter_Local dq 0x000042d68342622e
+Counter_Total dq 0x71000000ffffffff
+
+section .text
+
+Clove_Start:
+  push eax
+  push edx
+  push esi
+  mov esi, Counter_Local
+  rdtsc
+  mov dword [esi],eax
+  mov dword [esi+0x4],edx
+  pop esi
+  pop edx
+  pop eax
+  ; ret
+  jmp $ + 0x12345678
+
+Clove_End:
+  sub     esp,0Ch
+  push    eax
+  push    ebx
+  push    ecx
+  push    edx
+  push    ebp
+  push    esi
+  push    edi
+  rdtsc
+  mov     ebp,eax
+  mov     dword [esp+20h], Counter_Total
+  mov     ecx, Counter_Local
+  sub     ebp,dword [ecx]
+  mov     eax,edx
+  sbb     eax,dword [ecx+0x4]
+  mov     dword [esp+24h],ebp
+  mov     dword [esp+1Ch],eax
+
+label_0x30:
+  mov     ecx, Counter_Total
+  mov     edi,dword [ecx]
+  mov     ebx,edi
+  mov     esi,dword [ecx+0x4]
+  add     ebx,ebp
+  mov     ecx,esi
+  mov     edx,esi
+  adc     ecx,eax
+  mov     eax,edi
+  mov     ebp,dword [esp+20h] ; Counter_Total
+  lock cmpxchg8b [ebp]
+  mov     ebp,dword [esp+24h]
+  cmp     eax,edi
+  mov     eax,dword [esp+1Ch]
+  jne     label_0x30
+
+  cmp     edx,esi
+  jne     label_0x30
+
+  pop     edi
+  pop     esi
+  pop     ebp
+  pop     edx
+  pop     ecx
+  pop     ebx
+  pop     eax
+  add     esp,0Ch
+  ; ret
+  jmp $ + 0x12345678
--- clove/src/measurement.cpp
+++ clove/src/measurement.cpp
@@ -6,6 +6,7 @@
 void Log(LPCWSTR format, ...);
 
 struct MeasurementPack final : public CodePack {
+#ifdef _WIN64
   static constexpr uint8_t Template_MeasurementStart[] = {
     0x50, 0x52, 0x0f, 0x31, 0x48, 0xc1, 0xe2, 0x20,
     0x48, 0x09, 0xd0, 0x48, 0xba, 0x00, 0xa0, 0xa2,
@@ -27,6 +28,38 @@ struct MeasurementPack final : public CodePack {
   static constexpr uint32_t offset_MeasurementEnd_Counter_Local = 0x0d;
   static constexpr uint32_t offset_MeasurementEnd_Counter_Total = 0x1a;
   static constexpr uint32_t offset_MeasurementEnd_FinalJump = 0x29;
+#else
+  static constexpr uint8_t Template_Measurement32[] = {
+    // Start
+    0x50, 0x52, 0x56, 0xbe, 0x00, 0x10, 0x7b, 0x5c,
+    0x0f, 0x31, 0x89, 0x06, 0x89, 0x56, 0x04, 0x5e,
+    0x5a, 0x58,
+    0xe9, 0x73, 0x56, 0x34, 0x12,
+    // End
+    0x81, 0xec, 0x0c, 0x00, 0x00, 0x00, 0x50, 0x53,
+    0x51, 0x52, 0x55, 0x56, 0x57, 0x0f, 0x31, 0x89,
+    0xc5, 0xc7, 0x44, 0x24, 0x20, 0x08, 0x10, 0x7b,
+    0x5c, 0xb9, 0x00, 0x10, 0x7b, 0x5c, 0x2b, 0x29,
+    0x89, 0xd0, 0x1b, 0x41, 0x04, 0x89, 0x6c, 0x24,
+    0x24, 0x89, 0x44, 0x24, 0x1c, 0xb9, 0x08, 0x10,
+    0x7b, 0x5c, 0x8b, 0x39, 0x89, 0xfb, 0x8b, 0x71,
+    0x04, 0x01, 0xeb, 0x89, 0xf1, 0x89, 0xf2, 0x11,
+    0xc1, 0x89, 0xf8, 0x8b, 0x6c, 0x24, 0x20, 0xf0,
+    0x0f, 0xc7, 0x4d, 0x00, 0x8b, 0x6c, 0x24, 0x24,
+    0x39, 0xf8, 0x8b, 0x44, 0x24, 0x1c, 0x75, 0xd5,
+    0x39, 0xf2, 0x75, 0xd1, 0x5f, 0x5e, 0x5d, 0x5a,
+    0x59, 0x5b, 0x58, 0x81, 0xc4, 0x0c, 0x00, 0x00,
+    0x00,
+    0xe9, 0x73, 0x56, 0x34, 0x12,
+  };
+  static constexpr uint32_t offset_Measurement32_S_Counter_Local = 0x04;
+  static constexpr uint32_t offset_Measurement32_S_FinalJump = 0x12;
+  static constexpr uint32_t offset_Measurement32_E = 0x17;
+  static constexpr uint32_t offset_Measurement32_E_Counter_Local = 0x31;
+  static constexpr uint32_t offset_Measurement32_E_Counter_Total1 = 0x2c;
+  static constexpr uint32_t offset_Measurement32_E_Counter_Total2 = 0x45;
+  static constexpr uint32_t offset_Measurement32_E_FinalJump = 0x80;
+#endif
 
   uint64_t Couter_Total;
   uint64_t Couter_Local;
@@ -50,6 +83,7 @@ struct MeasurementPack final : public CodePack {
   {}
 
   bool ActivateDetourInternal(ExecutablePages &exec_pages) {
+#ifdef _WIN64
     MeasurementStart_Detour = exec_pages.Push(*this, MeasurementStart_Target);
     MeasurementEnd_Detour = at<void>(MeasurementStart_Detour,
                                      sizeof(Template_MeasurementStart)
@@ -69,6 +103,26 @@ struct MeasurementPack final : public CodePack {
         exec_pages.Revert(MeasurementStart_Detour);
       }
     }
+#else
+    MeasurementStart_Detour = exec_pages.Push(*this, MeasurementStart_Target);
+    MeasurementEnd_Detour = at<void>(MeasurementStart_Detour,
+                                     offset_Measurement32_E);
+    bool ret = DetourAttachHelper(MeasurementStart_Trampoline,
+                                  MeasurementStart_Detour)
+               && DetourAttachHelper(MeasurementEnd_Trampoline,
+                                     MeasurementEnd_Detour);
+    if (ret) {
+      ret = PutImmediateNearJump(at<void>(MeasurementStart_Detour,
+                                          offset_Measurement32_S_FinalJump),
+                                 MeasurementStart_Trampoline)
+            && PutImmediateNearJump(at<void>(MeasurementStart_Detour,
+                                             offset_Measurement32_E_FinalJump),
+                                    MeasurementEnd_Trampoline);
+      if (!ret) {
+        exec_pages.Revert(MeasurementStart_Detour);
+      }
+    }
+#endif
     return ret;
   }
 
@@ -80,9 +134,13 @@ struct MeasurementPack final : public CodePack {
   }
 
   size_t Size() const {
+#ifdef _WIN64
     return sizeof(Template_MeasurementStart)
            + sizeof(Template_MeasurementEnd)
            + 1;
+#else
+    return sizeof(Template_Measurement32);
+#endif
   }
 
   void Print() const {
@@ -93,6 +151,7 @@ struct MeasurementPack final : public CodePack {
   }
 
   void CopyTo(uint8_t *destination) const {
+#ifdef _WIN64
     std::memcpy(destination,
                 Template_MeasurementStart,
                 sizeof(Template_MeasurementStart));
@@ -109,6 +168,18 @@ struct MeasurementPack final : public CodePack {
       = &Couter_Local;
     *at<const void*>(destination, offset_MeasurementEnd_Counter_Total)
       = &Couter_Total;
+#else
+    std::memcpy(destination,
+                Template_Measurement32,
+                sizeof(Template_Measurement32));
+
+    *at<const void*>(destination, offset_Measurement32_S_Counter_Local)
+      = *at<const void*>(destination, offset_Measurement32_E_Counter_Local)
+        = &Couter_Local;
+    *at<const void*>(destination, offset_Measurement32_E_Counter_Total1)
+      = *at<const void*>(destination, offset_Measurement32_E_Counter_Total2)
+        = &Couter_Total;
+#endif
   }
 };
 
