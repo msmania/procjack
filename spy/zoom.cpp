bca61d19f37368bd30f09e513c68af808d653587
Toshihito Kikuchi <k.toshihito@yahoo.de>
2020-04-28 08:18:44 -0700

    SetWindowHookEx approach

--- spy/zoom.cpp
+++ spy/zoom.cpp
@@ -5,6 +5,11 @@
 #include "../common.h"
 #include "blob.h"
 
+constexpr UINT ZOOM_START_SHARING = 0x465;
+constexpr UINT ZOOM_STOP_SHARING = 0x466;
+constexpr UINT ZOOM_SHARE_MONITOR = 0x468;
+constexpr UINT ZOOM_SHARE_WINDOW = 0x469;
+
 //#define ZOOM_LOG 1
 
 void Log(LPCWSTR format, ...);
@@ -246,11 +251,6 @@ namespace hook {
 
   void* orig_SendMessageW;
   LRESULT WINAPI SendMessageW(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam) {
-    constexpr UINT ZOOM_START_SHARING = 0x465;
-    constexpr UINT ZOOM_STOP_SHARING = 0x466;
-    constexpr UINT ZOOM_SHARE_MONITOR = 0x468;
-    constexpr UINT ZOOM_SHARE_WINDOW = 0x469;
-
     switch (Msg) {
     case ZOOM_START_SHARING:
       Log(L">> Zoom: Desktop sharing has started!\n");
@@ -385,4 +385,28 @@ void HookZoom2(Package *package) {
   for (;;) {
     Sleep(100);
   }
+}
+
+LRESULT HookProc(int code, WPARAM wParam, LPARAM lParam) {
+  if (auto msg = reinterpret_cast<PCWPSTRUCT>(lParam)) {
+    switch (msg->message) {
+    case ZOOM_START_SHARING:
+      Log(L">> Zoom: Desktop sharing has started!\n");
+      break;
+    case ZOOM_STOP_SHARING:
+      Log(L">> Zoom: Desktop sharing has stopped!\n");
+      break;
+    case ZOOM_SHARE_MONITOR:
+      if (lParam) {
+        Log(L">> Zoom: source=%s\n", reinterpret_cast<LPCWSTR>(msg->lParam));
+      }
+      break;
+    case ZOOM_SHARE_WINDOW:
+      if (lParam) {
+        Log(L">> Zoom: source=HWND %p\n", *reinterpret_cast<HWND*>(msg->lParam));
+      }
+      break;
+    }
+  }
+  return CallNextHookEx(/*hhk*/nullptr, code, wParam, lParam);
 }
